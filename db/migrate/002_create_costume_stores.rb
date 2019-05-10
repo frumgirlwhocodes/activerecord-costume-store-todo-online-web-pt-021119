278617c7470596f9c7b2ef8bcbe4810f1fec0cc8
lieby weintraub <liebahinda@gmail.com>
2019-05-10 21:16:24 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,5 +1,7 @@
 # Create your costume_stores migration here
 class CreateCostumeStores < ActiveRecord::Migration[5.1]
   def change 
-    create_table :costume_stores
+    create_table :costume_stores do |t| 
+      t.text :name 
+      t.
 end 
